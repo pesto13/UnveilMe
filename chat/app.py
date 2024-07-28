3d63e0e6a0837de540c8241eda9e0adcfc19eb48
pesto13 <ricola1304@gmail.com>
2024-07-28 13:36:37 +0200

    incredibile

--- chat/app.py
+++ chat/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
@@ -6,11 +6,12 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 users = {}
+rooms = set()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', rooms=list(rooms))
 
 
 @app.route('/room/<room>/<username>')
@@ -18,15 +19,22 @@ def room(room, username):
     return render_template('room.html', room=room, username=username)
 
 
+@app.route('/rooms')
+def get_rooms():
+    return jsonify(list(rooms))
+
+
 @socketio.on('join')
 def handle_join(data):
     room = data['room']
     username = data['username']
     join_room(room)
+    rooms.add(room)
     if room not in users:
         users[room] = {}
     users[room][username] = 'inactive'
     emit('update_users', users[room], room=room)
+    emit('update_rooms', list(rooms), broadcast=True)
 
 
 @socketio.on('leave')
@@ -36,7 +44,11 @@ def handle_leave(data):
     leave_room(room)
     if room in users and username in users[room]:
         del users[room][username]
+        if not users[room]:  # If the room is empty, remove it
+            del users[room]
+            rooms.remove(room)
         emit('update_users', users[room], room=room)
+        emit('update_rooms', list(rooms), broadcast=True)
 
 
 @socketio.on('toggle_status')
@@ -46,7 +58,3 @@ def handle_toggle_status(data):
     if room in users and username in users[room]:
         users[room][username] = 'active' if users[room][username] == 'inactive' else 'inactive'
         emit('update_users', users[room], room=room)
-
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from app import create_app, socketio
 
-app = create_app()
+from chat.app import app, socketio
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
