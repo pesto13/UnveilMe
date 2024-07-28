8065eb5d776a59467be223c5818b12cfa99f10f1
pesto13 <ricola1304@gmail.com>
2024-07-28 14:47:49 +0200

    renaming

--- UnveilMe/app.py
+++ UnveilMe/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
